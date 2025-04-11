b733b85fd686a9e7c153dc8ebd976415c70c29dd
psangwon62 <psangwon62@gmail.com>
2025-04-11 17:07:38 +0900

    chore: tuist target 생성 확장 메서드 작성

--- Tuist/ProjectDescriptionHelpers.swift
+++ Tuist/ProjectDescriptionHelpers.swift
@@ -1,53 +1,80 @@
 import ProjectDescription
 
+extension String {
+    func firstChararacterUppercased() -> String {
+        guard let first = first else {
+            return ""
+        }
+        let rest = dropFirst()
+        return String(first).uppercased() + rest
+    }
+}
+
+extension Target {
+    enum TargetType: String {
+        case interface
+        case feature
+        case tests
+        case testing
+        case example
+
+        var product: Product {
+            switch self {
+                case .interface, .feature, .testing: .framework
+                case .tests: .unitTests
+                case .example: .app
+            }
+        }
+
+        func targetName(_ name: String) -> String {
+            return "\(name)\(rawValue.firstChararacterUppercased())"
+        }
+
+        func bundleId(name: String) -> String {
+            return "com.sangwon.\(name).\(rawValue)"
+        }
+
+        var targetSources: SourceFilesList {
+            switch self {
+                case .feature: "Sources/**"
+                default: "\(rawValue.firstChararacterUppercased())/**"
+            }
+        }
+
+        func targetDependencies(name: String, dependencies: [TargetDependency] = []) -> [TargetDependency] {
+            switch self {
+                case .interface: return dependencies
+                case .feature: return [.target(name: TargetType.interface.targetName(name))]
+                case .tests: return [.target(name: TargetType.feature.targetName(name)),
+                                     .target(name: TargetType.testing.targetName(name))]
+                case .testing: return [.target(name: TargetType.interface.targetName(name))]
+                case .example: return [.target(name: TargetType.feature.targetName(name)),
+                                       .target(name: TargetType.testing.targetName(name))]
+            }
+        }
+    }
+
+    static func target(name: String, type: TargetType, dependencies: [TargetDependency] = []) -> Target {
+        return .target(
+            name: type.targetName(name),
+            destinations: .iOS,
+            product: type.product,
+            bundleId: type.bundleId(name: name),
+            deploymentTargets: .iOS("16.0"),
+            sources: type.targetSources,
+            dependencies: type.targetDependencies(name: name, dependencies: dependencies)
+        )
+    }
+}
+
 extension Project {
     static func module(name: String, dependencies: [TargetDependency] = []) -> Project {
         let targets: [Target] = [
-            .target(
-                name: "\(name)Interface",
-                destinations: .iOS,
-                product: .framework,
-                bundleId: "com.example.\(name).interface",
-                deploymentTargets: .iOS("16.0"),
-                sources: "Interface/**",
-                dependencies: dependencies
-            ),
-            .target(
-                name: "\(name)",
-                destinations: .iOS,
-                product: .framework,
-                bundleId: "com.example.\(name)",
-                deploymentTargets: .iOS("16.0"),
-                sources: "Sources/**",
-                dependencies: []
-            ),
-            .target(
-                name: "\(name)Tests",
-                destinations: .iOS,
-                product: .unitTests,
-                bundleId: "com.example.\(name).tests",
-                deploymentTargets: .iOS("16.0"),
-                sources: "Tests/**",
-                dependencies: [.target(name: "\(name)")]
-            ),
-            .target(
-                name: "\(name)Testing",
-                destinations: .iOS,
-                product: .framework,
-                bundleId: "com.example.\(name).testing",
-                deploymentTargets: .iOS("16.0"),
-                sources: "Testing/**",
-                dependencies: [.target(name: "\(name)Interface")]
-            ),
-            .target(
-                name: "\(name)Example",
-                destinations: .iOS,
-                product: .app,
-                bundleId: "com.example.\(name).example",
-                deploymentTargets: .iOS("16.0"),
-                sources: "Tests/SnapshotTests/**",
-                dependencies: [.target(name: "\(name)")]
-            ),
+            .target(name: name, type: .interface, dependencies: dependencies),
+            .target(name: name, type: .feature),
+            .target(name: name, type: .tests),
+            .target(name: name, type: .testing),
+            .target(name: name, type: .example),
         ]
         return Project(name: name, targets: targets)
     }
