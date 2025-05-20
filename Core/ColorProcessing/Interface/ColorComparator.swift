f96d8bee985ce927a3f9fbd0ce1274ba78972c22
psangwon62 <psangwon62@gmail.com>
2025-05-21 00:09:02 +0900

    chore: Comparator 프로토콜 todo 추가

--- Core/ColorProcessing/Interface/ColorComparator.swift
+++ Core/ColorProcessing/Interface/ColorComparator.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// TODO: - 거리 비교 가능한 색상 타입을 통틀어서 사용할 수 있는 프로토콜(임시: Measureable)로 함수 간략화
 public protocol ColorComparator {
     func difference(between color1: UIColor, and color2: UIColor) async -> CGFloat
     func difference(between lab1: CIELAB, and lab2: CIELAB) -> CGFloat
